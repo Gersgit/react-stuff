69c757148edf8658b2c9daf78e8d891202ca230e
German Tsatsura <ger@Germans-MacBook-Pro.local>
2022-11-19 21:20:18 -0700

    stylesProject complete

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+// import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  const goalInputChangeHandler = event => {
+//   label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     ${(props) =>
+//       props.isValid
+//         ? `
+//         color: red;
+//     `
+//         : ""};
+//   }
+
+//   input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid #ccc;
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//     ${(props) =>
+//       props.isValid
+//         ? `
+//         border-color: red;
+//         background: #ffd7d7;
+//     `
+//         : ""};
+//   }
+
+//   input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+//   &.invalid input {
+//     border-color: red;
+//     background: #ffd7d7;
+//   }
+
+//   &.invalid label {
+//     color: red;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValue, setIsValue] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length !== 0) {
+      setIsValue(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValue(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
+    setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div
+        className={`${styles["form-control"]} ${!isValue && styles.invalid}`}
+      >
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+          onClick={() => setIsValue(true)}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
